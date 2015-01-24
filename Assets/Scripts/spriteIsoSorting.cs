ad27d68f9a31da3cb0fdb0e93bd0da0d2b60e71a
Benjamin Hamrick <benthesim@gmail.com>
2015-01-24 14:44:49 -0500

    stuff

---
+++ Assets/Scripts/spriteIsoSorting.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class spriteIsoSorting : MonoBehaviour {
+
+    int layer = 0;
+    SpriteRenderer spriteRenderer;
+	// Use this for initialization
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    
+	// Update is called once per frame
+	void Update () {
+        if (layer != -(int)((collider2D.bounds.center.y - collider2D.bounds.extents.y) * 100f))
+        {
+            layer = -(int)((collider2D.bounds.center.y - collider2D.bounds.extents.y) * 100f);
+            spriteRenderer.sortingOrder = layer;
+        }
+	}
+}
