24f31f4139a8ffc324ea2931a20110bbba179191
Benjamin Hamrick <benthesim@gmail.com>
2015-01-25 11:45:47 -0500

    -added grave controller

---
+++ Assets/Scripts/GraveController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+using UnityEngine.UI;
+
+public class GraveController : MonoBehaviour {
+
+    CharacterController playerToRevive;
+    CharacterController playerDoingReviving;
+    int reviveState;
+
+    public Slider slider;
+
+    float time;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+	    if(InputManager.Devices[playerDoingReviving.playerIndex].Action1)
+        {
+            if (time > .1f)
+            {
+                time = 0f;
+                slider.value = reviveState;
+                reviveState+=5;
+            }
+        }
+        else
+        {
+            slider.value = reviveState;
+            reviveState = 0;
+        }
+	}
+
+    public void SetPlayer(CharacterController controller)
+    {
+        playerToRevive = controller;
+    }
+
+    public void IncrementRevive(int amount)
+    {
+        reviveState += amount;
+        if(reviveState >= 100)
+        {
+            playerToRevive.gameObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<CharacterController>() != null && playerDoingReviving == null)
+        {
+            playerDoingReviving = other.GetComponent<CharacterController>();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<CharacterController>() != null && playerDoingReviving != null)
+        {
+            playerDoingReviving = null;
+        }
+    }
+}
