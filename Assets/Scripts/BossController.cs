98a4ead9809e025145d14a0ff471ddf735b8ae07
David Dempsey <davecdempsey@gmail.com>
2015-01-25 14:20:38 -0500

    - Working On Boss

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BossController : MonoBehaviour {
+
+	public float health;
+
+	public float maxHealth;
+
+	public List<GameObject> tenList;
+
+	public List<GameObject> hoardSpawners;
+
+	public 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
